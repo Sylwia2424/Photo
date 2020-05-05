cc557ead121fc15b7c2b27d02c77ed000593c72b
Sylwia <sylwia_24_24@wp.pl>
2020-05-05 15:21:09 +0200

    Add max lenght, fix issue

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -9,12 +9,26 @@ exports.add = async (req, res) => {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    if(title && author && email && file) { // if fields are not empty...
+    const patternTitle = new RegExp(/(([A-z]|[0-9]|\s|\.|\,)*)/, 'g');
+    const patternAuthor = new RegExp(/(([A-z]|\s|\.)*)/, 'g');
+    const patternEmail = new RegExp(/([A-z]|[0-9]|\-|\_|\+|\.|\,)+@([A-z]|[0-9])+\.([A-z])+/, 'g')
+    
+    const titleMatched = title.match(patternTitle).join('');
+    const authorMatched = author.match(patternAuthor).join('');
+    const emailMatched = email.match(patternEmail).join('');
 
-      //const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+    if(textMatched.length < text.length) throw new Error('Invalid characters...');
+
+    if(title && title.split('').length <= 25 
+      && titleMatched.length >= title.length 
+      && author && author.split('').length 
+      && authorMatched.length < author.length 
+      && email && emailMatched.length < email.length && file) { // if fields are not empty...
+
+      const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
-      if (ext === 'png' || ext === 'jpg' || ext === 'gif' ){
-        const newPhoto = new Photo({ title, author, email, src: fileExt, votes: 0 });
+      if (fileExt === 'png' || fileExt === 'jpg' || fileExt === 'gif' ){
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
         await newPhoto.save(); // ...save new photo in DB
         res.json(newPhoto);
       } else {
@@ -51,8 +65,8 @@ exports.vote = async (req, res) => {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
-      const vote = await Voter.findOne({user: req.clientIp});
-      if(!vote) {
+      const photoVote = await Voter.findOne({user: req.clientIp});
+      if(!photoVote) {
         const newVoter = new Voter({user: req.clientIp});
         newVoter.votes.push(photoToUpdate._id);
         await newVoter.save();
--- models/Photo.model.js
+++ models/Photo.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const photoSchema = new mongoose.Schema({
-  title: { type: String, required: true,},
+  title: { type: String, required: true},
   author: { type: String, required: true },
   email: { type: String, required: true },
   src: { type: String, required: true },
